e678fccc0fd1ec1eef9d449eb18e768448495eab
Giacomo Sansone <giacomo.sansone@yahoo.com>
2024-03-29 16:10:31 +0100

    Added macros

--- src/memory/memory_map.h
+++ src/memory/memory_map.h
@@ -10,4 +10,20 @@
 #define IF_SERIAL 0b00001000
 #define IF_JOYPAD 0b00010000
 
+#define OAM_INIT_ADDR 0xFE00
+
+#define PPU_BASE 0xff40
+#define PPU_LCDC 0xff40
+#define PPU_STAT 0xff41
+#define PPU_SCY  0xff42
+#define PPU_SCX  0xff43
+#define PPU_LY   0xff44
+#define PPU_LYC  0xff45
+#define PPU_DMA  0xff46
+#define PPU_BGP  0xff47
+#define PPU_OBP0 0xff48
+#define PPU_OBP1 0xff49
+#define PPU_WX   0xff4a
+#define PPU_WY   0xff4b
+
 #endif // !__MEMORY_MAP
